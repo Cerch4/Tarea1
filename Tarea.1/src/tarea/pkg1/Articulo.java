f5c65bb6f37b455deae51987afc6776278bb7c3e
Cerch4 <cefranco2021@inf.udec.cl>
2022-09-29 19:07:58 -0300

    clase articulo

---
+++ Tarea.1/src/tarea/pkg1/Articulo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea.pkg1;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Articulo {
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    public Articulo(float k, String nom, String desc, float pre){
+        peso = k;
+        nombre = nom;
+        descripcion = desc;
+        precio = pre;
+    }
+}
+
