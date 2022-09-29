2a3485097423074ebe377abd683725826c35cbe6
P0rotos <sebgarcia2021@inf.udec.cl>
2022-09-29 20:30:26 -0300

    CPago1

---
+++ Tarea.1/src/tarea/pkg1/Pago.java
@@ -0,0 +1,53 @@
+
+package tarea.pkg1;
+import java.util.Date;
+import java.util.ArrayList;
+public class Pago {
+    private float monto;
+    private Date fecha;
+    public Pago(float monto, int year, int month, int day){
+        this.monto = monto;
+        fecha = new Date(year,month,day);
+    }
+    public void setmonto(float monto){
+        this.monto = monto;
+    }
+    public float getmonto(){
+        return monto;
+    }    
+    public void setfecha(int year, int month, int day){
+        fecha = new Date(year,month,day);
+    }
+    public Date getfecha(){
+        return fecha;
+    }
+}
+
+class Efectivo extends Pago{
+    public Efectivo(float monto, int year, int month, int day){
+        super(monto, year, month, day);
+    }
+    public float calcDevolucion(){
+        return super.getmonto()-calcPrecio(); //corregir cuando este listo calcPrecio
+    }
+}
+
+class Transferencia extends Pago{
+    private String banco;
+    private String numCuenta;
+    public Transferencia(String banco, String numCuenta, float monto, int year, int month, int day){
+        super(monto, year, month, day);
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+}
+
+class Tarjeta extends Pago{
+    private String tipo;
+    private String numTransaccion;
+    public Tarjeta(String tipo, String numTransaccion, float monto, int year, int month, int day){
+        super(monto, year, month, day);
+        this.tipo = tipo;
+        this.numTransaccion = numTransaccion;
+    }
+}
