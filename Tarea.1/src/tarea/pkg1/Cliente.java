ea1eb6236b166dc10ebf5d238561778ec18cf789
P0rotos <114672332+P0rotos@users.noreply.github.com>
2022-10-08 17:51:11 -0300

    Update Cliente.java

--- Tarea.1/src/tarea/pkg1/Cliente.java
+++ Tarea.1/src/tarea/pkg1/Cliente.java
@@ -2,12 +2,16 @@ package tarea.pkg1;
 public class Cliente {
     private String nombre;
     private String rut;
-    public Cliente(String nombre, String rut){
+    private Direccion direccion;
+    public Cliente(String nombre, String rut, Direccion direccion){
         this.nombre = nombre;
         this.rut = rut;
+        this.direccion = direccion;
     }
     public String getnombre(){return nombre; }
     public String getrut(){return rut; }
+    public String getdireccion(){return direccion; }
     public void setnombre(String nombre){this.nombre = nombre;}
     public void setrut(String rut){this.rut = rut;}
+    public void setdireccion(Direccion direccion){this.direccion = direccion;}
 }
